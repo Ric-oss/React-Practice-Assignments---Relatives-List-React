8c3450dcd91448beffeeb0a6a98c5b0b2cc14ef0
Richa <18tiwariricha@gmail.com>
2021-05-19 22:55:39 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 //const relative=[{name:"mama"},{name:"masi"},{name:"chacha"}];
 class App extends Component {
- relative=[{key:"relativeListItem1",name:"mama"},{key:"relativeListItem2",name:"masi"},{key:"relativeListItem1",name:"chacha"}];
+    relative = [{ key: "relativeListItem1", name: "mama" }, { key: "relativeListItem2", name: "masi" }, { key: "relativeListItem1", name: "chacha" }];
 
-  listitem = this.relative.map((value)=>{
-      return <li key={value.key}>{value.name}</li>
-  })
 
     render() {
 
-        return(
+        return (
             <div id="main">
-                <ol type="1" key="relative">{this.listitem}</ol>
-               {/* Do not remove the main div */}
+                <ol type="1" key="relative">
+
+                    {this.relative.map((value) => {
+                        return (
+                            <li key={value.key}>{value.name}</li>);
+                    })
+                    }
+                </ol>
+                {/* Do not remove the main div */}
             </div>
         )
     }
