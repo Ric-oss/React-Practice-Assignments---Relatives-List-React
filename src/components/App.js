3a39f38190fa1f942b83b46f260a1659f8665207
Richa <18tiwariricha@gmail.com>
2021-05-19 22:41:50 +0530

    donenew

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,10 @@ import '../styles/App.css';
 
 //const relative=[{name:"mama"},{name:"masi"},{name:"chacha"}];
 class App extends Component {
- relative=[{relativeListItem:1,name:"mama"},{irelativeListItem:2,name:"masi"},{relativeListItem:3,name:"chacha"}];
+ relative=[{key:"relativeListItem1",name:"mama"},{key:"relativeListItem2",name:"masi"},{key:"relativeListItem1",name:"chacha"}];
 
   listitem = this.relative.map((value)=>{
-      return <li key={value.relativeListItem}>{value.name}</li>
+      return <li key={value.key}>{value.name}</li>
   })
 
     render() {
