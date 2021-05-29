50dacb9841dd3abe6ef02ac06a5c4acbd6bf5505
Ric-oss <63895838+Ric-oss@users.noreply.github.com>
2021-05-29 15:12:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,23 @@
-import React, { Component, useState } from "react";
+import React, {Component, useState} from "react";
 import '../styles/App.css';
-
-//const relative=[{name:"mama"},{name:"masi"},{name:"chacha"}];
 class App extends Component {
-
-
     render() {
-        const relativeList = [{ key: "relativeListItem1", name: "mama" },
-        { key: "relativeListItem2", name: "masi" },
-        { key: "relativeListItem3", name: "chacha" }];
-
-        return (
-            <div id="main">
-                <ol type="1" key="relative">
-
-                    {relative.map((value) => {
-                        return (
-                            <li key={value.key}>{value.name}</li>);
-                    })
-                    }
-                </ol>
-                {/* Do not remove the main div */}
-            </div>
-        )
-    }
+        const relativeList = [
+            {name:'Mumbai',key:'relativeListItem1'},
+            {name:'Pune',key:'relativeListItem2'},
+            {name:'Nasik',key:'relativeListItem3'}
+        ];
+        return(
+            <ol type='1' key='relativeList'>
+            {
+                relativeList.map(relative=>{
+                return (
+                <li key={relative.key}>{relative.name}</li>
+        );
+            })
+            }
+            </ol>
+        )    }
 }
 
 
